6fb161082f52b4920722d297ad9f13a00b04f696
Lucas Moraes <lucas.moraes@avenuecode.com>
2020-10-18 18:22:39 -0300

    Adicionando produtos

--- scripts/galleria.js
+++ scripts/galleria.js
@@ -2,3 +2,25 @@
     Galleria.loadTheme('https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js');
     Galleria.run('.galleria');
 }());
+
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+})
